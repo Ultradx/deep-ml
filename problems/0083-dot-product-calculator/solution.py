db1925188b3a65356775dc669e23a798689735ea
Ultradx <37597102+Ultradx@users.noreply.github.com>
2026-09-23 10:51:57 +0000

    Solve: Dot Product Calculator

---
+++ problems/0083-dot-product-calculator/solution.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def calculate_dot_product(vec1, vec2):
+	"""
+	Calculate the dot product of two vectors.
+	Args:
+		vec1 (numpy.ndarray): 1D array representing the first vector.
+		vec2 (numpy.ndarray): 1D array representing the second vector.
+	Returns:
+		The dot product of the two vectors.
+	"""
+	# Your code here
+	dot_product = 0
+	for i in range(0,len(vec1)):
+		dot_product += vec1[i]*vec2[i]
+	return dot_product
